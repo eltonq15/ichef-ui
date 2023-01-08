bbaccd0a3ae5ff733eaf0888ac71021f0866c0c4
elton <eltonq15@gmail.com>
2023-01-08 14:25:24 -0300

    Improve UI and add features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import { useRecipe } from "./api/recipeAPI";
 
 const App = () => {
   const [ingredients, setIngredients] = useState("");
-  const {data: recipe, loading, error} = useRecipe(ingredients);
+  const [recipe, setRecipe] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const { data, loading, error } = useRecipe(ingredients);
 
-  const ingredientsRef = useRef();
+  useEffect(() => {
+    if (data) {
+      setRecipe(data.data);
+    }
+  }, [data]);
+
+  const ingredientsRef = useRef(null);
+
+  const handleChange = () => {
+    setDisabled(ingredientsRef.current.value?.length === 0);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      generateRecipe();
+    }
+  };
 
   const generateRecipe = () => {
     setIngredients(ingredientsRef.current.value);
@@ -17,21 +35,39 @@ const App = () => {
       <div className="card">
         <h1 className="title">iChef</h1>
         <p className="description">
-          Not sure what to cook? <br/>Type the ingredients you have and we'll give
-          you the best recipe we can find!
+          Not sure what to cook? <br />
+          Type the ingredients you have and we'll give you the best recipe we
+          can find!
         </p>
-        <input ref={ingredientsRef} className="ingredients-input" type="text" placeholder="i.e. Rice, beans, egg and beef" />
-        <button className="search-btn" onClick={generateRecipe} >Search</button>
+        <input
+          ref={ingredientsRef}
+          onChange={handleChange}
+          className="ingredients-input"
+          type="text"
+          placeholder="i.e. Rice, beans, egg and beef"
+          id="ingredients"
+          name="ingredients"
+          onKeyDown={handleKeyDown}
+        />
+        <button
+          className="search-btn"
+          disabled={disabled}
+          onClick={generateRecipe}
+        >
+          Search
+        </button>
         {loading && <div>Loading...</div>}
         {error && <div>Error on generating recipe :(</div>}
-        <div className="recipe">
-          {recipe && <div>
-            {recipe}
-          </div>}
-        </div>
+        {recipe && (
+          <textarea
+            readOnly
+            className="recipe"
+            defaultValue={recipe}
+          ></textarea>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/api/recipeAPI.js
+++ src/api/recipeAPI.js
@@ -1,14 +1,22 @@
 import axios from "axios";
-import {useQuery} from "react-query";
+import { useQuery } from "react-query";
 
 export const useRecipe = (ingredients) => {
-    const {data, loading, error} = useQuery("recipe", () => {
-        if (!ingredients) {
-            return {data: null, loading: true, error: null};
-        }
-        const text = `create a recipe with title, ingredients and instructions, including the ingredients: ${ingredients}. Return fallback message if any of the previous items is not a valid ingredient.`;
-        return axios.get(`http://localhost:3001/recipe?question=${text}`);
+  return useQuery(
+    ["recipe", ingredients],
+    () => {
+      if (!ingredients) {
+        return;
+      }
+      console.log("ingredients: ", ingredients);
+      const text = `create a recipe with title, ingredients and instructions, including the ingredients: ${ingredients}. Return fallback message if any of the previous items is not a valid ingredient.`;
+      return axios.get(
+        `https://ichef-server.herokuapp.com/recipe?question=${text}`
+      );
+    },
+    {
+      enabled: !!ingredients,
+      refetchOnWindowFocus: false,
     }
   );
-  return {data, loading, error};
-}
+};
