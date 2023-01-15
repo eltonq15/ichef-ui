51f3d1baae28a4e49a2bfa401150bfa550a828e9
Elton Alves Ribeiro <59945102+eltonq15@users.noreply.github.com>
2023-01-15 09:43:46 +0000

    Add language constant to query

--- src/api/recipeAPI.js
+++ src/api/recipeAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useQuery } from "react-query";
-import { RECIPE_MSG } from "../constants";
+import { RECIPE_MSG, LANGUAGE_MSG } from "../constants";
 
 export const useRecipe = (ingredients, language = 'en') => {
   return useQuery(
@@ -9,7 +9,7 @@ export const useRecipe = (ingredients, language = 'en') => {
       if (!ingredients) {
         return;
       }
-      const text = `${RECIPE_MSG[language]} ${ingredients}`;
+      const text = `${RECIPE_MSG[language]} ${ingredients} ${LANGUAGE_MSG[language]}`;
       return axios.get(
         `https://ichef-server.herokuapp.com/recipe?question=${text}`
       );
